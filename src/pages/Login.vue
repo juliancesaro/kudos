a5ec0c711b06d30e9a556eae383fd47bb022bdb8
juliancesaro <julianjcesaro@gmail.com>
2021-05-02 13:38:40 +1000

    Added server for Heroku / remved GitHub pages

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -15,15 +15,9 @@ export default {
   methods: {
     login() {
       const { VUE_APP_CLIENT_ID } = process.env
-      const redirectUrl =
-        process.env.NODE_ENV === 'production'
-          ? 'https://juliancesaro.github.io/kudos/profile'
-          : 'http://localhost:8080/profile'
+      const redirectUrl = 'http://localhost:8080/profile'
       window.location = `http://www.strava.com/oauth/authorize?client_id=${VUE_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,activity:read`
     },
-  },
-  mounted() {
-    console.log(process.env.NODE_ENV)
   },
 }
 </script>
